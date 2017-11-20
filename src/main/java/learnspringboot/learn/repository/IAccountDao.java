709b85b340a0093ad293283297822ec3c843ebdd
CNsmallC <381726676@qq.com>
2017-11-20 17:13:31 +0800

    Initial commit

---
+++ src/main/java/learnspringboot/learn/repository/IAccountDao.java
@@ -0,0 +1,18 @@
+package learnspringboot.learn.repository;
+
+import learnspringboot.learn.entity.Account;
+
+import java.util.List;
+
+public interface IAccountDao {
+    int add(Account account);
+
+    int update(Account account);
+
+    int delete(int id);
+
+    Account findAccountByID(int id);
+
+    List<Account> findAccountList();
+
+}
